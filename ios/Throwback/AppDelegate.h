394b792d5b47a81e68b6de75ad73e3074951a701
Adrien Truong <adrien.truong@me.com>
2016-02-13 05:43:36 -0800

    create xcode project

---
+++ ios/Throwback/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Throwback
+//
+//  Created by Adrien Truong on 13/02/2016.
+//  Copyright © 2016 Adrien Truong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
